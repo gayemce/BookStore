eef9d2351917a4cdc8927fa5417979cfec3a92ae
gayemce <100995170+gayemce@users.noreply.github.com>
2023-10-09 19:58:18 +0300

    .

---
+++ BookStoreClient/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
+import { CategoryPipe } from './pipes/category.pipe';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutsComponent,
+    HomeComponent,
+    NavbarComponent,
+    CategoryPipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ BookStoreServer/BookStoreServer.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using GSF.FuzzyStrings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace BookStoreServer.WebApi.Controllers;
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        string searchTerm = "hllr";
+        var categories = SeedData.Categories;
+        var closeMatches = categories.Where(c => c.Name.ApproximatelyEquals(searchTerm, 
+            FuzzyStringComparisonOptions.UseJaccardDistance, FuzzyStringComparisonTolerance.Normal)).ToList();
+
+        return Ok(SeedData.Categories);
+    }
+}
